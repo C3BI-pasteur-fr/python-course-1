ab256851c7a3eb2311817c82bf7ee60c531a205d
Bertrand Néron <bneron@pasteur.fr>
2014-08-29 00:28:50 +0200

    flip name between fib_recursive_cache and cache_decorator

--- source/_static/code/fib_recursive_cache.py
+++ source/_static/code/fib_recursive_cache.py
@@ -1,36 +1,22 @@
-def memoize(func):
-    cache = {}
-    def inner_func(arg):
+def fib_gen():
+    cache = {0 : 0,
+             1 : 1}
+
+    def fib(n):
+        # from http://en.literateprograms.org/Fibonacci_numbers_(Python)
         if n in cache:
-            return cache(n)
+            return cache[n]
         else:
-            res = func(*args, **kwargs)
-            cache[arg] = res
+            res =  fib(n-1) + fib(n-2)
+            cache[n] = res
             return res
-    return inner_func
+    return fib
 
-@memoize
-def fib(n):
-    # from http://en.literateprograms.org/Fibonacci_numbers_(Python)
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n -1)
-    
-factorial = memoize(factorial) 
-    
-    
 if __name__ == '__main__':
     import sys
     
     n = int(sys.argv[1])
+    fib = fib_gen()
     resultat = fib(n)
     print resultat
