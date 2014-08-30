7a9a68285faa0fa3952e5733fdda1888014c0719
Bertrand Néron <bneron@pasteur.fr>
2014-08-30 03:05:09 +0200

    add print to see the cache in action

--- source/_static/code/fib_recursive_cache.py
+++ source/_static/code/fib_recursive_cache.py
@@ -5,10 +5,13 @@ def fib_gen():
     def fib(n):
         # from http://en.literateprograms.org/Fibonacci_numbers_(Python)
         if n in cache:
+            print "get {0} : {1} from cache".format(n, cache[n])
             return cache[n]
         else:
+            print "call fib({0}) + fib({1})".format(n-1, n-2)
             res =  fib(n-1) + fib(n-2)
             cache[n] = res
+            print "put {0} : {1} in cache".format(n, res)
             return res
     return fib
 
