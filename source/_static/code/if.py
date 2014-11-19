5237dcc291734bdce53d01ff5be8928f57af243e
Bertrand Néron <bneron@pasteur.fr>
2014-11-19 16:38:15 +0100

    improve print statement to distinguish clearly the two cases

--- source/_static/code/if.py
+++ source/_static/code/if.py
@@ -1,6 +1,6 @@
 i = 10
 
-print "if elif"
+print "if elif statement"
 if i > 1:
    print "i > 1"
 elif i > 2:
@@ -8,7 +8,7 @@ elif i > 2:
 elif i > 3:
    print "i > 3"
 
-print "suite of if"
+print "suite of if statements"
 if i > 1:
    print "i > 1"
 if i > 2:
